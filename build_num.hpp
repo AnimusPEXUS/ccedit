d790e23e54f9b9c144c6dc7a9222b3929f7c8d8f
AnimusPEXUS <animuspexusagutilities@gmail.com>
2024-12-23 01:22:57 +0300

    build num up

--- build_num.hpp
+++ build_num.hpp
@@ -1,22 +1,23 @@
-#ifndef WAYROUND_I2P_20241210_171354_655666
-#define WAYROUND_I2P_20241210_171354_655666
+#ifndef WAYROUND_I2P_20241208_155643_188113
+#define WAYROUND_I2P_20241208_155643_188113
 
-namespace wayround_i2p::ccedit
-{
+namespace wayround_i2p::ccedit {
 
-const unsigned int ccedit_version[] = {
-    2024,
-    12,
-    8,
-    16,
-    10,
-    58
+const unsigned int ccedit_version[] = { 
+2024,
+12,
+22,
+22,
+21,
+45
 };
 
-const std::string ccedit_version_string =
-    "2024.12.8."
-    "16.10.58";
+const std::string ccedit_version_string = 
+	"2024.12.22."
+	"22.21.45"
+	;
 
-} // namespace wayround_i2p::ccedit
+}
+
 
 #endif
